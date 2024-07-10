42d603c2e07e84fb57efbde5954f181dc82e0f6e
Zreon <56314835+zreonx@users.noreply.github.com>
2024-07-10 20:17:51 +0800

    Project Setup, added scroll animation progress and theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+//Import Components
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+//ThemeProvider
+import ThemeProvider from "@/context/ThemeProvider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,8 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='light'>
+          <Header />
+          {children}
+          <Footer />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ app/template.tsx
@@ -0,0 +1,41 @@
+"use client";
+//Framer Motion
+import { motion } from "framer-motion";
+
+//Hooks
+import useScrollProgress from "@/hooks/useScrollProgress";
+import { Hind_Madurai } from "next/font/google";
+
+//Variants
+const variants = {
+  hidden: { opacity: 0 },
+  enter: { opacity: 1 },
+};
+
+type TemplateType = {
+  children: React.ReactNode;
+};
+const Template = ({ children }: TemplateType) => {
+  const completion = useScrollProgress();
+
+  return (
+    <>
+      <motion.main
+        variants={variants}
+        initial='hidden'
+        animate='enter'
+        transition={{ type: "linear", delay: 0.2, duration: 0.4 }}
+      >
+        {children}
+      </motion.main>
+      <span
+        style={{ transform: `translateY(${completion - 100}%)` }}
+        className='fixed z-50 bg-primary w-1 top-0 right-0 bottom-0 transition-all duration-700'
+      ></span>
+
+      <div className='h-[3500px]'></div>
+    </>
+  );
+};
+
+export default Template;
